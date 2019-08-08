ad671680244eef3079b54cfd710e5d7f70c782fb
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-08-07 22:00:10 -0300

    criando script para a execuçao do nodemon com o yarn

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req, res) =>{
-    return res.send(`Ola DEV ${req.query.nome}!`);
+    return res.json({mensage: "Opaaaaaa"});
 });
 
 server.listen(3333);
