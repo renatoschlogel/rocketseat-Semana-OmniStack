5d2d65bd02d143548630f9564bbef3bf9d479c47
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-08-08 00:45:41 -0300

    salvando os dados do Dev no banco de dados

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,11 +1,26 @@
 const axios = require('axios');
+const Dev = require('../models/Dev');
 
 module.exports = {
     async store(req, res){
         const {username} = req.body;
 
+        const userExists = await Dev.findOne({user: username});
+        if (userExists){
+            return res.json(userExists);
+        }
+
         const response = await axios.get(`https://api.github.com/users/${username}`);
+
+        var {name, bio, avatar_url } = response.data;
+        
+        const dev = await Dev.create({
+            nome: name,
+            user: username,
+            bio: bio,
+            avatar: avatar_url
+        });
     
-        return res.json({data: response.data});
+        return res.json(dev);
     }
 }
