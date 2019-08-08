1487482a7acb18379e3ae869c168f6bdc471d73c
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-08-07 23:37:31 -0300

    conexao com o banco de dados

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const server = express();
+
+mongoose.connect('mongodb+srv://renato:renato@cluster0-14h8x.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+   useNewUrlParser: true 
+});
 
 server.use(express.json()); // para trabalhar com Json 
 server.use(routes);
