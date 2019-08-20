431a40e65ae2c8725f7888fa904120ef3a41960a
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-08-20 00:12:10 -0300

    ajustes...

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,84 @@
 import React from 'react';
+import './Main.css';
+
+import logo from '../assets/logo.svg';
+import iconLike from '../assets/like.svg';
+import iconDislike from '../assets/dislike.svg';
 
 export default function Main({match}){
     return (
-        <h1>isUsuário: {match.params.id}</h1>
+        <div className="main-container">
+            <img src={logo} alt="Tindev"/>
+            <ul>
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/6994181?v=4" alt=""/>
+                    <footer>
+                        <strong>Renato W Schlogel</strong>
+                        <p>Analista de Sistemas </p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={iconDislike} alt="Dislike"/>
+                        </button>
+                        <button type="button">
+                            <img src={iconLike} alt="Like"/>
+                        </button>
+                    </div>
+                </li>
+
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/6994181?v=4" alt=""/>
+                    <footer>
+                        <strong>Renato W Schlogel</strong>
+                        <p>Analista de Sistemas </p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={iconDislike} alt="Dislike"/>
+                        </button>
+                        <button type="button">
+                            <img src={iconLike} alt="Like"/>
+                        </button>
+                    </div>
+                </li>
+
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/6994181?v=4" alt=""/>
+                    <footer>
+                        <strong>Renato W Schlogel</strong>
+                        <p>Analista de Sistemas </p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={iconDislike} alt="Dislike"/>
+                        </button>
+                        <button type="button">
+                            <img src={iconLike} alt="Like"/>
+                        </button>
+                    </div>
+                </li>
+
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/6994181?v=4" alt=""/>
+                    <footer>
+                        <strong>Renato W Schlogel</strong>
+                        <p>Analista de Sistemas </p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={iconDislike} alt="Dislike"/>
+                        </button>
+                        <button type="button">
+                            <img src={iconLike} alt="Like"/>
+                        </button>
+                    </div>
+                </li>
+            </ul>
+
+        </div>
     );
 }
